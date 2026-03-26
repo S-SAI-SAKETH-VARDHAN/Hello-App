f99f8c80be954d739d6d18811c8e24143f0176c4
S-SAI-SAKETH-VARDHAN <as6770@srmist.edu.in>
2026-03-26 09:33:38 +0530

    Update HelloApp.java

--- HelloApp.java
+++ HelloApp.java
@@ -1,17 +1,23 @@
-public class HelloApp {
+class HelloApp {
     public static void main(String[] args) {
-        String result = "Hello ";
-
-        // Concatenate arguments with ", "
-        for (int i = 0; i < args.length; i++) {
-            result += args[i] + ", ";
+        
+        // Check if arguments are passed
+        if (args.length == 0) {
+            System.out.println("Hello");
+        } else {
+            String result = "Hello ";
+            
+            // Concatenate all arguments into a single string
+            for (int i = 0; i < args.length; i++) {
+                result += args[i];
+                
+                // Add space between words (avoid trailing space)
+                if (i != args.length - 1) {
+                    result += " ";
+                }
+            }
+            
+            System.out.println(result);
         }
-
-        // Remove trailing ", " using substring
-        if (args.length > 0) {
-            result = result.substring(0, result.length() - 2);
-        }
-
-        System.out.println(result);
     }
 }
